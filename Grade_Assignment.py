384b0e36f7a45110c5c6b03f87ed98b99844a213
HARINI <89853508+HARINI-RadheyKrishna@users.noreply.github.com>
2023-11-22 16:31:09 -0800

    Uploaded Grade_Assignment.py code file
    Created a Python code for assessing the grades of students

---
+++ Grade_Assignment.py
@@ -0,0 +1,35 @@
+
+def grade_assignment(file_to_be_read) -> str:   
+    with open(file_to_be_read, "r") as file1:
+        content = file1.readlines()
+
+    for line in content:
+
+        match = re.match(" (\d+)", line)
+
+        if match:
+            num = int(match.group(1))
+
+            if 0 <= num <= 10:
+                grade = "F"
+
+            elif 11 <= num <= 20:
+                grade = "D"
+
+            elif 21 <= num <= 30:
+                grade = "C"
+
+            elif 31 <= num <= 40:
+                grade = "B"
+
+            elif 41 <= num <= 50:
+                grade = "A"
+
+            else:
+                grade = "Not a valid score"
+        else:
+            continue
+        
+        return grade
+
+    return "Invalid file"
